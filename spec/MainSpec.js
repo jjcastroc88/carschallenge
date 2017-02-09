2fed5e0663883281655be659d3e72a633887507b
david-londono <davflondono@gmmail.com>
2017-02-09 13:54:50 -0500

    First commit
    First Commit cars challenge

---
+++ spec/MainSpec.js
@@ -0,0 +1,11 @@
+describe("Main", function() {
+  var Main = require('../js/Main');
+
+  beforeEach(function() {
+    cat = new Cat();
+  });
+  it("the object should be undefined", function() {
+    expect(candy)not
+  });
+
+});
